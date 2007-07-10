29eb2a0fa6629eb4836a6c025c9bd12b87b728b5
fox <fox@2ad329bf-350f-0410-9b45-bf18f469613a>
2007-07-10 10:42:58 +0000

    Code modularization to help external call from another python script. Add doc string
    git-svn-id: svn+ssh://localhost/home/fox/prog/depot/pytimeout@625 2ad329bf-350f-0410-9b45-bf18f469613a

--- pytimeout.py
+++ pytimeout.py
@@ -4,6 +4,11 @@
 # Sébastien Renard (sebastien.renard@digitalfox.org)
 # Code licensed under GNU GPL V2
 
+"""pytimeout module.
+Provide both a command line interface and an API (via the "run" function)
+to allow process launching with a define time to live before being killed.
+"""
+
 # Python imports
 import sys
 import os
@@ -11,28 +16,54 @@ from subprocess import Popen
 from time import sleep
 from optparse import OptionParser
 
-def main():
+# Some constant for default values
+TIMEOUT=10 # in seconds
+RETRY=2  # number of retry to kill process
 
+def main():
+    """Just a wrapper for CLI usage to the run() function"""
+    
     # Options & args stuf
     (options, argv)=parseOptions()
 
-    target=" ".join(argv)
-    print "running %s" % target
-
+    if not argv:
+        print "Command to launch must be supplied !"
+        sys.exit(1)
     
-    #TODO: define this in a separate function that could be easiliy called from
-    # another python program
+    target=" ".join(argv)
+    
+    run(target, options.timeout, options.retry, 
+        preserveChildren=options.preserveChildren, verbose=options.verbose)
+
+def run(target, timeout=TIMEOUT, retry=RETRY,
+        preserveChildren=False, verbose=False):
+    """Run a process but kill it after timeout
+    @param target: the command to launch (str)
+    @param timeout: timeout in seconds before start killing process (int)
+    @param retry: Number of time we will try to kill the process with SIGTERM and SIGKILL (int)
+    @param preserveChildren: Do we need to also kill process children ? Default is True (bool)
+    @param verbose: Print what happened on standard output (bool)
+    """
+    
+    if verbose:
+        print "running %s" % target
+
     process=Popen(target, shell=True)
-    sleep(options.timeout)
+    sleep(timeout)
     if process.pid is None:
-        print "process correctly finished"
+        if verbose:
+            print "process correctly finished"
     else:
-        pids=getChildrenPid(process.pid)
+        if preserveChildren:
+            pids=[]
+        else:
+            pids=getChildrenPid(process.pid)
         pids.append(process.pid)
-        for i in xrange(options.retry):
-            for signal in (15, 9):
+        for i in xrange(retry):
+            for signal in (15, 9): # SIGTERM then SIGKILL
                 for pid in pids:
-                    print "kill %s with signal %s" % (pid, signal)
+                    if verbose:
+                        print "kill %s with signal %s" % (pid, signal)
                     try:
                         os.kill(pid, signal)
                     except OSError, e:
@@ -47,7 +78,9 @@ def main():
 
 def getChildrenPid(fatherPid):
     """Return a list of a process children PID
-    @arg pid: father pid"""
+    @param pid: father pid
+    @return: list of children pid (list of int)"""
+
     # Get all PID
     pids=[int(pid) for pid in os.listdir("/proc") if pid.isdigit()]
     
@@ -70,20 +103,27 @@ def getChildrenPid(fatherPid):
     return children
         
 def parseOptions():
-    """Parses command argument using optparse python module"""
-    parser=OptionParser()
+    """Parses command argument using optparse python module
+    @return: (options, argv) tuple"""
+    
+    usage = "usage: %prog [options] <program to launch>"
+    parser=OptionParser(usage=usage)
 
     # Timeout
-    parser.add_option("-t", "--timeout", dest="timeout", type="int", default=10,
+    parser.add_option("-t", "--timeout", dest="timeout", type="int", default=TIMEOUT,
               help="Timeout in seconds. Default is 10")
 
     # Retries
-    parser.add_option("-r", "--retry", dest="retry", type="int", default=2,
+    parser.add_option("-r", "--retry", dest="retry", type="int", default=RETRY,
               help="Number of kill retries. Default is 2")
 
     # Preserve children
     parser.add_option("-p", "--preserve-children", dest="preserveChildren", action="store_true",
               help="Do not kill process children")
+
+    # Verbose
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
+              help="Tell what happened on standard output")
 
     return parser.parse_args()
 
